e6211fc78745eb16db84d241eb02043ca048eb4d
Nicole <jessinthehallway@gmail.com>
2024-09-05 12:45:11 +0800

    Further prepping separate debug handler

--- playbook4/header/pbkprojdebugerrorhandlers.h
+++ playbook4/header/pbkprojdebugerrorhandlers.h
@@ -5,7 +5,9 @@
 #include <string>
 #include <filesystem>
 
-int projDebugHandler(int invokeDebug, std::ofstream& debugFile, std::string debugType, std::string debugInfo, std::string debugFlag);
+// Only specific to project creation debugging. Debug file creation on `playbookcore.h`
+void projWriteoutDebugHandler(int invokeDebug, std::string debugType, std::string debugInfo, std::string debugFlag);
+int projDebugHandler(int invokeDebug, std::string debugType, std::string debugInfo, std::string debugFlag);
 
 #endif
 
