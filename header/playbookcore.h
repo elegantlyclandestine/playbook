6c58464927c80db8bdc1749632c9d2db882c99d3
Nicole Holloway <jessinthehallway@gmail.com>
2024-09-24 16:38:18 +0800

    Enum declaration

--- header/playbookcore.h
+++ header/playbookcore.h
@@ -8,6 +8,17 @@
 #include <ctime>
 #include <unistd.h>
 
+enum CLIArgument { VERSION, DEBUG, DEBUG_PRINTOUT, DEBUG_FULL, DEBUG_FULL_PRINTOUT, CREATE, UNDEFINED };
+CLIArgument getArgument(const std::string& input) {
+    if (input == "--version" || input == "--v") return VERSION;
+    if (input == "--debug" || input == "--db") return DEBUG;
+    if (input == "--debug-printout" || input == "--dbp") return DEBUG_PRINTOUT;
+    if (input == "--debug-full" || input == "--dbf") return DEBUG_FULL;
+    if (input == "--debug-full-printout" || input == "--dbfp") return DEBUG_FULL_PRINTOUT;
+    if (input == "create") return CREATE;
+    return UNDEFINED;
+};
+
 std::ofstream createDebugFile(std::string debugFlag);
 std::string getCurrentWorkingDir();
 std::unordered_map<std::string, std::string> loadConfig(const std::string& configFilePath);
